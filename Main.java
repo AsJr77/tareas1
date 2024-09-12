48d79070e7121e9315e454224ac082820e94983c
luis sanchez <l24121323@morelia.tecnm.mx>
2024-09-11 23:38:00 -0600

    H

---
+++ Main.java
@@ -0,0 +1,34 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+// Fórmula 1
+
+      double num1 = (12 + 8) * (5 - 3);
+      double num2 = 6.0 / 2;
+      double den1 = (3 * 4);
+      double den2 = (8.0 / 2);
+      double resultadoFormula1 = (num1 - num2) / (den1 + den2);
+      System.out.println("Resultado de la Fórmula 1: " + resultadoFormula1);
+
+
+      // Fórmula 2
+
+      double numerador = (((9 * 5) - (15 + 3) + (8 * 2) - (12.0 / 4)) * (7 + 2) - (6 * 3 / 2.0))
+              / (2 + (3 * 2.0) / (4 - 2)) + 5 * ((8 / 4.0) + 1) - (6 / (2 + 1));
+      double denominador = (((4 + 2) * (7 - 1)) / (3 + 1) + ((3 * 2) - (5 + 1)) * (8 / 2.0))
+              / (3 + (6 / 2.0) + (7 / 2.0)) + 2 * (3 + (8 / 4.0));
+      double resultado = numerador / denominador;
+      System.out.println("Resultado de la formula 2: " + resultado);
+
+
+
+
+
+      // Fórmula 3
+
+      double num3 =((6 + 4) * 3) - (18 / 2);
+      double den3 = (8 - 5) + (3 * 2);
+      double resultadoFormula3 = num3 / den3;
+      System.out.println("Resultado de la Fórmula 3: " + resultadoFormula3);
+
