cb9f7db337b1efa6f02a5f8cb4568ef5361edc92
AsJr77 <l24121323@morelia.tecnm.mx>
2024-09-20 12:58:04 -0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,34 +1,73 @@
+import java.util.Scanner;
+
+
 public class Main {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
 
 
-// Fórmula 1
+        System.out.println("Introduce las coordenadas del primer punto (x1, y1):");
+        double x1 = input.nextDouble();
+        double y1 = input.nextDouble();
+        System.out.println("Introduce las coordenadas del segundo punto (x2, y2):");
+        double x2 = input.nextDouble();
+        double y2 = input.nextDouble();
+        System.out.println("Introduce las coordenadas del tercer punto (x3, y3):");
+        double x3 = input.nextDouble();
+        double y3 = input.nextDouble();
 
-      double num1 = (12 + 8) * (5 - 3);
-      double num2 = 6.0 / 2;
-      double den1 = (3 * 4);
-      double den2 = (8.0 / 2);
-      double resultadoFormula1 = (num1 - num2) / (den1 + den2);
-      System.out.println("Resultado de la Fórmula 1: " + resultadoFormula1);
+        //  longitudes de los lados del triángulo
+        double lado1 = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+        double lado2 = Math.sqrt(Math.pow(x3 - x2, 2) + Math.pow(y3 - y2, 2));
+        double lado3 = Math.sqrt(Math.pow(x1 - x3, 2) + Math.pow(y1 - y3, 2));
+
+        // perímetro del triángulo
+        double perimetro = lado1 + lado2 + lado3;
+
+        // área del triángulo utilizando la fórmula de Herón
+        double s = perimetro / 2;
+        double area = Math.sqrt(s * (s - lado1) * (s - lado2) * (s - lado3));
 
 
-      // Fórmula 2
+        //altura del triángulo desde el primer vértice
+        double altura = (2 * area) / lado1;
 
-      double numerador = (((9 * 5) - (15 + 3) + (8 * 2) - (12.0 / 4)) * (7 + 2) - (6 * 3 / 2.0))
-              / (2 + (3 * 2.0) / (4 - 2)) + 5 * ((8 / 4.0) + 1) - (6 / (2 + 1));
-      double denominador = (((4 + 2) * (7 - 1)) / (3 + 1) + ((3 * 2) - (5 + 1)) * (8 / 2.0))
-              / (3 + (6 / 2.0) + (7 / 2.0)) + 2 * (3 + (8 / 4.0));
-      double resultado = numerador / denominador;
-      System.out.println("Resultado de la formula 2: " + resultado);
+        // ángulos internos del triángulo utilizando la ley de los cosenos
+        double anguloA = Math.toDegrees(Math.acos((Math.pow(lado2, 2) + Math.pow(lado3, 2) - Math.pow(lado1, 2)) / (2 * lado2 * lado3)));
+        double anguloB = Math.toDegrees(Math.acos((Math.pow(lado1, 2) + Math.pow(lado3, 2) - Math.pow(lado2, 2)) / (2 * lado1 * lado3)));
+        double anguloC = Math.toDegrees(Math.acos((Math.pow(lado1, 2) + Math.pow(lado2, 2) - Math.pow(lado3, 2)) / (2 * lado1 * lado2)));
+
+        // Verificar si el triángulo es equilátero
+        boolean esEquilatero = (lado1 == lado2) && (lado2 == lado3);
+
+        // punto medio de cada lado del triángulo
+        double puntoMedio1X = (x1 + x2) / 2;
+        double puntoMedio1Y = (y1 + y2) / 2;
+        double puntoMedio2X = (x2 + x3) / 2;
+        double puntoMedio2Y = (y2 + y3) / 2;
+        double puntoMedio3X = (x3 + x1) / 2;
+        double puntoMedio3Y = (y3 + y1) / 2;
 
 
+        System.out.println("Lado A: " + lado1);
+        System.out.println("Lado B: " + lado2);
+        System.out.println("Lado C: " + lado3);
+        System.out.println();
+        System.out.println("Perímetro del triángulo: " + perimetro);
+        System.out.println();
+        System.out.println("Área del triángulo: " + area);
+        System.out.println();
+        System.out.println("Altura desde el vértice (x1, y1): " + altura);
+        System.out.println();
+        System.out.println("Ángulo A: " + anguloA + " grados");
+        System.out.println("Ángulo B: " + anguloB + " grados");
+        System.out.println("Ángulo C: " + anguloC + " grados");
+        System.out.println();
+        System.out.println("¿El triángulo es equilátero? " + esEquilatero);
+        System.out.println();
+        System.out.println("Punto medio del lado AB: (" + puntoMedio1X + ", " + puntoMedio1Y + ")");
+        System.out.println("Punto medio del lado BC: (" + puntoMedio2X + ", " + puntoMedio2Y + ")");
+        System.out.println("Punto medio del lado CA: (" + puntoMedio3X + ", " + puntoMedio3Y + ")");
 
-
-
-      // Fórmula 3
-
-      double num3 =((6 + 4) * 3) - (18 / 2);
-      double den3 = (8 - 5) + (3 * 2);
-      double resultadoFormula3 = num3 / den3;
-      System.out.println("Resultado de la Fórmula 3: " + resultadoFormula3);
-
+    }
+}
