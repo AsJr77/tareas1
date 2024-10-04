87482bbd47a46083c92f2fc374ccdaa20405a1d7
AsJr77 <l24121323@morelia.tecnm.mx>
2024-10-03 20:35:29 -0600

    Update tare.java

---
+++ archivos/tare.java
@@ -0,0 +1,29 @@
+
+public class Main {
+    public static void main(String[] args) {
+
+       String correo = "juan.perez@empresa.com";
+       String Fechaascenso = "2023-04-15";
+       String salario = "50000.75";
+
+       String nombre = correo.substring(0,10);
+
+       String invertido = new StringBuilder(nombre).reverse().toString();
+
+       String empresa = correo.substring(10,22);
+
+       int a = 2023;
+       int b = 04;
+       int c = 15;
+       int suma = a + b + c;
+
+      double salarioo = 50000.75;
+      int entero = (int) salarioo;
+      double decimal = salarioo - entero;
+      double difAbsoluta = Math.abs(entero - decimal);
+      difAbsoluta = 74.75;
+      double resultadoo = difAbsoluta * 2;
+      System.out.println(invertido  + empresa + "|" + suma + "|" + resultadoo);
+
+    }
+}
