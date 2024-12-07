debe1d4bfc1b0f56f60d407378fcb2144e046651
AsJr77 <l24121323@morelia.tecnm.mx>
2024-12-06 19:22:41 -0600

    Update Ejercicio1.java

--- Ejercicio1.java
+++ Ejercicio1.java
@@ -1,6 +1,6 @@
 public class Ejercicio1 {
     public static void main(String[] args) {
-        // Pruebas con diferentes valores de Celsius
+        
         System.out.println("25°C -> " + celsiusAFahrenheit(25) + "°F");
         System.out.println("0°C -> " + celsiusAFahrenheit(0) + "°F");
         System.out.println("-10°C -> " + celsiusAFahrenheit(-10) + "°F");
